489fc68c141939d56926596ab12c242649e40ec1
Miguel Angel Rodriguez Ortiz <21000597@alumnos.utleon.edu.mx>
2024-09-21 10:09:27 -0600

    Commit sabado 21

--- src/app/product/product-list/product-list.component.ts
+++ src/app/product/product-list/product-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { IProductos } from '../productos';
 
 @Component({
   selector: 'app-product-list',     /* Nombre del componente */
@@ -7,38 +8,46 @@ import { Component } from '@angular/core';
 })
 export class ProductListComponent {
 
+  imageWidth:number=300;
+  imageMargin:number=2;
+  muestraImg:boolean=true;
+  listFilter:string=''
+
+  muestImg():void{
+    this.muestraImg=!this.muestraImg;
+  }
   //*ngIf= true o false
   
-  productos:any[]=[
+  productos:IProductos[]=[
     {
       "productoId":1,
       "Modelo":"Sentra",
       "Descripcion":"4 puertas",
       "Precio": 20000,
-      "Year":2023,
+      "Year":" marzo 23 2023",
       "Marca":"NISSAN",
       "Color":"Azul",
-      "ImagenUrl":"https://th.bing.com/th/id/OIP.yxqg01EsTpLP1IoF59T4NgHaE6?rs=1&pid=ImgDetMain.jpg"
+      "ImagenUrl":"https://th.bing.com/th/id/OIP.MEYuzZH191HKW04ukKLWBQHaFB?rs=1&pid=ImgDetMain"
     },
     {
       "productoId":2,
       "Modelo":"A4",
       "Descripcion":"4 puertas",
       "Precio": 30000,
-      "Year":2022,
+      "Year":"january 31 2022",
       "Marca":"AUDI",
       "Color":"Blanco",
-      "ImagenUrl":"https://th.bing.com/th/id/OIP.IkM1K_U6YcMGIjb7iXKobQHaEo?rs=1&pid=ImgDetMain.jpg"
+      "ImagenUrl":"https://th.bing.com/th/id/R.66957a83f115e2c97259fe78482e789b?rik=8lmkuzoVMlYbZg&pid=ImgRaw&r=0"
     },
     {
       "productoId":3,
       "Modelo":"Rio",
       "Descripcion":"4 puertas",
       "Precio": 150000,
-      "Year":2020,
-      "Marca":"KIA",
+      "Year":"Junio 14 2020",
+      "Marca":"BMW",
       "Color":"Rojo",
-      "ImagenUrl":"https://th.bing.com/th/id/OIP.L9REaQeu5ZIq5vKMpnMg-wHaFj?rs=1&pid=ImgDetMain.jpg"
+      "ImagenUrl":"https://th.bing.com/th/id/OIP.L7Mj5gNDwuxoJaUg1-QFMgHaE8?rs=1&pid=ImgDetMain"
     }
   ]
 
