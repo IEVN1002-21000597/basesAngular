a8d4fdd9afc95a2c9e3a1ec7e74251ab2f8f1222
Miguel Angel Rodriguez Ortiz <21000597@alumnos.utleon.edu.mx>
2024-10-04 20:08:36 -0600

    Trabajos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { CinepolisComponent } from './formularios/cinepolis/cinepolis.component';
+
 const routes: Routes = [];
 
 @NgModule({
